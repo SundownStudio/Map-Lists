5a6671944a9ec260e9d28bb3854ea98b4e6832a6
Sundown <sundown@superfivecard.com>
2015-08-13 18:04:11 -0400

    refactoring constants

---
+++ app/src/main/java/com/sundown/maplists/network/GeocodeConstants.java
@@ -0,0 +1,20 @@
+package com.sundown.maplists.network;
+
+/**
+ * Created by Sundown on 8/13/2015.
+ */
+public class GeocodeConstants {
+
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final int FROM_LATLNG = 50;
+    public static final int FROM_ADDRESS = 150;
+    public static final String GEO_OPERATION = "geo-op";
+    public static final String PACKAGE_NAME = "com.google.android.gms.location.sample.locationaddress";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
+    public static final String MAP_LATITUDE = "lat";
+    public static final String MAP_LONGITUDE = "lon";
+    public static final String MAP_ADDRESS = "address";
+    public static final String MAP_ERROR = "error";
+}
